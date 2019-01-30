c5c8544b315029d1e5160071c3b9b6fd10f1e52d
younggeun0 <34850791+younggeun0@users.noreply.github.com>
2019-01-30 09:40:05 +0900

    Create 07. insert skill data.sql

---
+++ 03.개발/temp_query/07. insert skill data.sql
@@ -0,0 +1,22 @@
+
+DELETE FROM skill;
+
+INSERT INTO skill(skill_num, skill_name)
+VALUES ('s_01','Java');
+INSERT INTO skill(skill_num, skill_name)
+VALUES ('s_02','JSP/Servlet');
+INSERT INTO skill(skill_num, skill_name)
+VALUES ('s_03','Spring');
+INSERT INTO skill(skill_num, skill_name)
+VALUES ('s_04','Oracle');
+INSERT INTO skill(skill_num, skill_name)
+VALUES ('s_05','HTML');
+INSERT INTO skill(skill_num, skill_name)
+VALUES ('s_06','CSS');
+INSERT INTO skill(skill_num, skill_name)
+VALUES ('s_07','Linux');
+INSERT INTO skill(skill_num, skill_name)
+VALUES ('s_08','JS');
+
+COMMIT;
+
