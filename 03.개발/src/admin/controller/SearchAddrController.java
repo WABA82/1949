b79f153c7c595fad4df9b3c07844b071a23c5f7d
younggeun0 <34850791+younggeun0@users.noreply.github.com>
2019-02-07 16:50:23 +0900

    Update 관리자메인, 주소창 View구현

--- 03.개발/src/admin/controller/SearchAddrController.java
+++ 03.개발/src/admin/controller/SearchAddrController.java
@@ -3,12 +3,25 @@ package admin.controller;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+import admin.view.SearchAddrView;
 
 public class SearchAddrController extends WindowAdapter implements ActionListener {
 
+	private SearchAddrView sav;
+	
+	public SearchAddrController(SearchAddrView sav) {
+		this.sav = sav;
+	}
+	
+	@Override
+	public void windowClosing(WindowEvent e) {
+		sav.dispose();
+	}
+	
 	@Override
 	public void actionPerformed(ActionEvent e) {
-
+		
 	}
-
 }
