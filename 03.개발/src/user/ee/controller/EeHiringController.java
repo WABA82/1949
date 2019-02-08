9fc057cb5ba40e4980da9c03015be951195ada0e
gkwl7878 <45455322+gkwl7878@users.noreply.github.com>
2019-02-08 19:32:15 +0900

    cm

--- 03.개발/src/user/ee/controller/EeHiringController.java
+++ 03.개발/src/user/ee/controller/EeHiringController.java
@@ -5,19 +5,77 @@ import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.util.List;
+
+import javax.swing.JOptionPane;
+import javax.swing.JTable;
+
+import user.dao.EeDAO;
+import user.ee.dto.EeHiringCdtDTO;
+import user.ee.view.EeDetailSearchView;
+import user.ee.view.EeHiringView;
+import user.ee.vo.EeHiringVO;
 
 public class EeHiringController extends WindowAdapter implements ActionListener, MouseListener {
-
-	@Override
-	public void mouseClicked(MouseEvent e) {
-
-	}
-
-	@Override
-	public void actionPerformed(ActionEvent e) {
-
+	private EeHiringView ehv;
+	private List<EeHiringVO> list;
+	private String eeId;
+	private EeHiringCdtDTO dhcdto;
+	private EeDAO e_dao;
+	private int count; 
+	public EeHiringController(EeHiringView ehv) {
+		this.ehv = ehv;
+		
 	}
 	
+	@Override
+	public void mouseClicked(MouseEvent me) {
+		switch(me.getClickCount()) {
+		case 2:
+			if(me.getSource()==ehv.getJtErInfo()) {
+				JTable jt = ehv.getJtErInfo();
+				System.out.println(jt.getSelectedRow());
+				
+			}
+		}
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent ae) {
+		if(ae.getSource()==ehv.getJbDetailSearch()) {
+			EeDetailSearchView edsv = new EeDetailSearchView(ehv, this);
+		}
+		
+		if(ae.getSource()==ehv.getJbWordSearch()) {
+			//검색을 누르면
+			
+			//++ 추가할것: 요기 검색할 때 dao에서 메소드 불러서 삭제한 데이터 다시 살려내기
+			for(int i=0; i<ehv.getJtErInfo().getRowCount();i++) {
+				if(ehv.getJtErInfo().getValueAt(i, 3).toString().equals(ehv.getJtfSearch().getText().trim())) {
+					count++;
+				}else {
+					ehv.getDtmErInfo().removeRow(i);
+				}
+			}//end for
+			
+			if(count!=0) {
+				JOptionPane.showMessageDialog(ehv, "총"+count+"의 결과가 있습니다.");
+			}
+		}
+		if(ae.getSource()==ehv.getJcbSort()) {
+			//나열하는 콤보박스
+		}
+		if(ae.getSource()==ehv.getJtErInfo()) {
+			//텍스트필드 엔터를 누르면
+		}
+	}
+	
+	@Override
+	public void windowClosing(WindowEvent we) {
+		ehv.dispose();
+	}
+
 	@Override
 	public void mousePressed(MouseEvent e) {}
 	@Override
--- 03.개발/src/user/ee/view/EeHiringView.java
+++ 03.개발/src/user/ee/view/EeHiringView.java
@@ -14,6 +14,7 @@ import javax.swing.JTextField;
 import javax.swing.table.DefaultTableModel;
 
 import user.common.vo.EeMainVO;
+import user.ee.controller.EeHiringController;
 import user.ee.vo.EeHiringVO;
 
 @SuppressWarnings("serial")
@@ -23,17 +24,23 @@ public class EeHiringView extends JDialog {
 	private JComboBox<String> jcbSort;
 	private JTable jtErInfo;
 	private DefaultTableModel dtmErInfo;
+	private JTextField jtfSearch;
 	
-	
-	public EeHiringView(EeMainView emv, List<EeHiringVO> ehvo, String s) {
+	public EeHiringView(EeMainView emv, List<EeHiringVO> eh_vo, String s) {
 		super(emv,"구인 정보 보기",true);
 				
 		String[] eeColumns= {"번호","구인정보번호","제목","회사명","직급","근무지역","학력","고용형태","급여","등록일"};
-		dtmErInfo=new DefaultTableModel(eeColumns, 40) {
+		dtmErInfo=new DefaultTableModel(eeColumns, 0) {
 			public boolean isCellEditable(int row, int column) {
 				return false;
 			}
 		};
+		///////////가데이터//////////////////
+		//"번호","구인정보번호","제목","회사명","직급","근무지역","학력","고용형태","급여","등록일"
+		Object[] rowData1 = {"1","er_000001",	"테스트제목1",	"1", "C","서울", "C", "Y", "2500","2019-01-30 오후 7:06:43"};
+		Object[] rowData2 = {"2","er_000002",	"테스트제목2",	"2", "C", "서울", "C", "Y", "2300", "2019-01-29 오후 7:06:42"};		
+		dtmErInfo.addRow(rowData1);
+		dtmErInfo.addRow(rowData2);
 		
 		String sort[]= {"등록일순","직급순","급여순"};
 		jcbSort=new JComboBox<String>(sort);
@@ -44,7 +51,7 @@ public class EeHiringView extends JDialog {
 		jbDetailSearch=new JButton("조건검색");
 		
 		JLabel jlSearch=new JLabel("기업명 검색");
-		JTextField jtfSearch=new JTextField();
+		jtfSearch = new JTextField();
 		
 		jbWordSearch=new JButton("검색");
 		
@@ -63,53 +70,57 @@ public class EeHiringView extends JDialog {
 		add(jlSearch);
 		add(jtfSearch);
 		add(jbWordSearch);
-	
+		
+		
+		EeHiringController ehc = new EeHiringController(this);
+		jcbSort.addActionListener(ehc);
+		jbDetailSearch.addActionListener(ehc);
+		jtfSearch.addActionListener(ehc);
+		jbWordSearch.addActionListener(ehc);
+		jtErInfo.addMouseListener(ehc);
+		addWindowListener(ehc);
+		
 		setBounds(100, 100, 1000, 500);
 		setResizable(false);
 		setVisible(true);
-		setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
 		
 	}//ErHiringView
 
 	
 
-	public JButton getJbDetailSearch() {
-		return jbDetailSearch;
+	public JTextField getJtfSearch() {
+		return jtfSearch;
 	}
 
 
 
-	public JButton getJbSearch() {
+	public JTable getJtErInfo() {
+		return jtErInfo;
+	}
+
+	public JButton getJbWordSearch() {
 		return jbWordSearch;
 	}
 
+	public DefaultTableModel getDtmErInfo() {
+		return dtmErInfo;
+	}
 
+	public JButton getJbDetailSearch() {
+		return jbDetailSearch;
+	}
 
 	public JComboBox<String> getJcbSort() {
 		return jcbSort;
 	}
 
-
-
-	public JTable getjtErInfo() {
-		return jtErInfo;
-	}
-
-
-
-	public DefaultTableModel getdtmErInfo() {
-		return dtmErInfo;
-	}
-
-
-
-/*	public static void main(String[] args) {
+	public static void main(String[] args) {
 		EeMainVO em_vo= new EeMainVO("ds", "sdf", "ds", "asd");
 		EeMainView emv=new EeMainView(em_vo);
 		List<EeHiringVO> ehvo=new ArrayList<EeHiringVO>();
 		String s=new String();
 		new EeHiringView(emv, ehvo, s);
-	}*/
+	}
 	
 	
 }//class
