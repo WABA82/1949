e8759df9014b7634504fe23ab47d8244c5cf1011
hyewon0218 <esc0218@naver.com>
2019-02-12 05:56:56 +0900

    vo수정

--- 03.개발/src/user/common/vo/FindIdVO.java
+++ 03.개발/src/user/common/vo/FindIdVO.java
@@ -22,29 +22,6 @@ public class FindIdVO {
 		return "FindIdVO [name=" + name + ", tel=" + tel + "]";
 	}
 	
+}	
+
 	
-
-	private String name, tel;
-
-	public FindIdVO(String name, String tel) {
-		super();
-		this.name = name;
-		this.tel = tel;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public String getTel() {
-		return tel;
-	}
-
-	@Override
-	public String toString() {
-		return "FindIdVO [name=" + name + ", tel=" + tel + "]";
-	}
-	
-	
-	
-}
