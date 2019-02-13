b803d6ba2f177eb8d86f9ee92884a01f153cc988
WABA82 <43511145+WABA82@users.noreply.github.com>
2019-02-14 08:59:01 +0900

    재현커밋

--- 03.개발/src/user/er/controller/ErInterestController.java
+++ 03.개발/src/user/er/controller/ErInterestController.java
@@ -6,11 +6,13 @@ import java.awt.event.WindowAdapter;
 import java.sql.SQLException;
 import java.util.List;
 
+import javax.swing.ImageIcon;
 import javax.swing.JOptionPane;
+import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
 
 import user.dao.ErDAO;
-import user.ee.vo.EeInterestVO;
+import user.er.view.ErDetailEeView;
 import user.er.view.ErInterestView;
 import user.er.vo.ErInterestVO;
 
@@ -19,6 +21,7 @@ public class ErInterestController extends WindowAdapter implements MouseListener
 	private ErInterestView eriv;
 	private String er_id;
 	private ErDAO er_dao;
+	private final int DBL_CLICK = 2;
 
 	public ErInterestController(ErInterestView eriv, String er_id) {
 		this.eriv = eriv;
@@ -27,6 +30,11 @@ public class ErInterestController extends WindowAdapter implements MouseListener
 		setDTM(er_id);
 	}// 생성자
 
+	/**
+	 * 로그인 되어있는 기업사용자의 id를 사용해 DB에서 관심구직자 목록을 가져와 화면에 목록으로 출력하는 일.
+	 * 
+	 * @param er_id
+	 */
 	private void setDTM(String er_id) {
 		DefaultTableModel dtmErInfo = eriv.getDtmEeInfo();
 		dtmErInfo.setRowCount(0); // DTM 0으로 초기화.
@@ -36,26 +44,28 @@ public class ErInterestController extends WindowAdapter implements MouseListener
 			List<ErInterestVO> list = er_dao.selectInterestEEInfoList(er_id);
 
 			// JTable에 조회한 정보를 출력.
-			EeInterestVO eivo = null;
+			ErInterestVO eivo = null;
+			String imgPath = "C:/dev/1949/03.개발/가데이터/구직자사진/150x200px/";
 
 			Object[] rowData = null;
 			for (int i = 0; i < list.size(); i++) {
 
 				/* list에 담겨진 VO객체로 EeInterestVO객체 생성하기 */
-				//eivo = list.get(i);
+				eivo = list.get(i);
 
 				// DTM에 데이터를 추가하기 위한 일차원배열(Vector)을 생성하고 데이터를 추가
-				rowData = new Object[10];
+				rowData = new Object[11];
 				rowData[0] = new Integer(i + 1);
-				rowData[1] = eivo.getErNum();
-				rowData[2] = eivo.getSubject();
-				rowData[3] = eivo.getCoName();
+				rowData[1] = eivo.getEe_num();
+				rowData[2] = new ImageIcon(imgPath + eivo.getImg());
+				rowData[3] = eivo.getName();
 				rowData[4] = eivo.getRank();
 				rowData[5] = eivo.getLoc();
 				rowData[6] = eivo.getEducation();
-				rowData[7] = eivo.getHireType();
-				rowData[8] = new Integer(eivo.getSal());
-				rowData[9] = eivo.getInputDate();
+				rowData[7] = new Integer(eivo.getAge());
+				rowData[8] = eivo.getPortfolio();
+				rowData[9] = eivo.getGender();
+				rowData[10] = eivo.getInput_date();
 
 				// DTM에 추가
 				dtmErInfo.addRow(rowData);
@@ -69,11 +79,24 @@ public class ErInterestController extends WindowAdapter implements MouseListener
 			JOptionPane.showMessageDialog(eriv, "DB에서 데이터를 받아오는 중 문제 발생...");
 			e.printStackTrace();
 		} // end catch
-		
+
 	}// setDTM
 
+	/**
+	 * 더블클릭했을 때 실행될 메소드.
+	 */
 	@Override
-	public void mouseClicked(MouseEvent e) {
+	public void mouseClicked(MouseEvent me) {
+		switch (me.getClickCount()) {
+		case DBL_CLICK:
+
+			if (me.getSource() == eriv.getJtEeInfo()) {
+				JTable jt = eriv.getJtEeInfo(); // 테이블 받아오기
+				String ee_num = ((String) jt.getValueAt(jt.getSelectedRow(), 1));
+				new ErDetailEeView(eriv, er_id, ee_num);
+			} // end if
+
+		}// end switch
 	}// mouseClicked
 
 	////////// 안쓰는 메소드 //////////
