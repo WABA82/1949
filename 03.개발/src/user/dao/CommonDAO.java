177a4addca6a672164a72f7290c3481a8afb7d79
WABA82 <43511145+WABA82@users.noreply.github.com>
2019-02-14 20:41:16 +0900

    재현커밋

--- 03.개발/src/user/dao/CommonDAO.java
+++ 03.개발/src/user/dao/CommonDAO.java
@@ -207,58 +207,58 @@ public class CommonDAO {
 	
 	}//updatePass
 	
-	public UserInfoVO selectUserInfo(String id) throws SQLException {
-		String userInfo="";
-		
-		Connection con=null;
-		PreparedStatement stmt=null;
-		ResultSet rs=null;
-		
-		try {
-			con=getConn();
-			String selectUserInfo="select name, tel, seq, zipcode, addr1, addr2, email from user_info where id=? ";
-		}finally {
-			
-		}
-		
-		
-		
-		return userInfo;
-	}
+//	public UserInfoVO selectUserInfo(String id) throws SQLException {
+//		String userInfo="";
+//		
+//		Connection con=null;
+//		PreparedStatement stmt=null;
+//		ResultSet rs=null;
+//		
+//		try {
+//			con=getConn();
+//			String selectUserInfo="select name, tel, seq, zipcode, addr1, addr2, email from user_info where id=? ";
+//		}finally {
+//
+//		}
+//		
+//		
+//		
+//		return userInfo;
+//	}
 
 	
 	
 	
+	/* 위에 같은이름 메소드가 하나 더 있습니다. 확인하고 하나 지워주세요. */
+//	public EeMainVO selectEeMain(String id) throws SQLException {
+//		EeMainVO emvo=null;
+//		
+//		Connection con =null;
+//		PreparedStatement pstmt =null;
+//		ResultSet rs = null;
+//		
+//		try {
+//		con =getConn();
+//		
+//		StringBuilder selectEeInfo = new StringBuilder();
+//		selectEeInfo.append("select ut.id, ut.name, ei.img, ut.activation").append("from ee_info ei,USER_TABLE ut")
+//			.append("where ut.id=ei.ee_id").append("and ut.id=?");
+////		String selectEeInfo = "SELECT EE_ID, NAME, IMG, ACTIVATION FROM EE_INFO WHERE ID=?";
+//		pstmt = con.prepareStatement(selectEeInfo.toString());
+//		
+//		pstmt.setString(1, id);
+//		
+//		rs=pstmt.executeQuery();
+//			if(rs.next()) {
+//				emvo = new EeMainVO(rs.getString("id"), rs.getString("name"),rs.getString("img"), rs.getString("activation"));
+//			}
+//		}finally {
+//		if(rs!=null) {	rs.close();	}
+//		if(pstmt!=null) {pstmt.close();}
+//		if(con!=null) {con.close();}
+//		}
+//		
+//		return emvo;
+//	}//selectEeMain
 	
-	public EeMainVO selectEeMain(String id) throws SQLException {
-		EeMainVO emvo=null;
-		
-		Connection con =null;
-		PreparedStatement pstmt =null;
-		ResultSet rs = null;
-		
-		try {
-		con =getConn();
-		
-		StringBuilder selectEeInfo = new StringBuilder();
-		selectEeInfo.append("select ut.id, ut.name, ei.img, ut.activation").append("from ee_info ei,USER_TABLE ut")
-			.append("where ut.id=ei.ee_id").append("and ut.id=?");
-//		String selectEeInfo = "SELECT EE_ID, NAME, IMG, ACTIVATION FROM EE_INFO WHERE ID=?";
-		pstmt = con.prepareStatement(selectEeInfo.toString());
-		
-		pstmt.setString(1, id);
-		
-		rs=pstmt.executeQuery();
-			if(rs.next()) {
-				emvo = new EeMainVO(rs.getString("id"), rs.getString("name"),rs.getString("img"), rs.getString("activation"));
-			}
-		}finally {
-		if(rs!=null) {	rs.close();	}
-		if(pstmt!=null) {pstmt.close();}
-		if(con!=null) {con.close();}
-		}
-		
-		return emvo;
-	}//selectEeMain
-	
-}
+}// class
