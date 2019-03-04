8928373e08fcfb74bee9d6b113e02138c9e60d28
younggeun0 <34850791+younggeun0@users.noreply.github.com>
2019-03-04 17:38:08 +0900

    Update 로그인 TextField 탭으로 이동
    정미가 도와줘서 구현

--- 03.개발/src/admin/controller/LoginController.java
+++ 03.개발/src/admin/controller/LoginController.java
@@ -2,6 +2,8 @@ package admin.controller;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
@@ -10,7 +12,7 @@ import javax.swing.JOptionPane;
 import admin.view.AdminMainView;
 import admin.view.LoginView;
 
-public class LoginController extends WindowAdapter implements ActionListener {
+public class LoginController extends WindowAdapter implements ActionListener, KeyListener {
 	
 	private LoginView lv;
 	private int cnt;
@@ -21,8 +23,16 @@ public class LoginController extends WindowAdapter implements ActionListener {
 	}
 	
 	@Override
+	public void keyPressed(KeyEvent e) {
+		if (e.getKeyCode() == KeyEvent.VK_TAB) {
+			lv.getJpfPass().requestFocus();
+		}
+	}
+	
+	@Override
 	public void actionPerformed(ActionEvent e) {
-		if(e.getSource() == lv.getJbLogin()) {
+		if(e.getSource() == lv.getJbLogin() || e.getSource() == lv.getJtfId() 
+				|| e.getSource()  == lv.getJpfPass()) {
 			login();
 		}
 	}
@@ -66,4 +76,10 @@ public class LoginController extends WindowAdapter implements ActionListener {
 	public void windowClosing(WindowEvent e) {
 		lv.dispose();
 	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {}
+
+	@Override
+	public void keyReleased(KeyEvent e) {}
 }
