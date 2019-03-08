5c49d20ca6a648a96f91181b8bf429d0bf509ef1
younggeun0 <34850791+younggeun0@users.noreply.github.com>
2019-03-08 17:02:28 +0900

    Update Admin Log변경

--- 03.개발/src/admin/controller/CoModifyController.java
+++ 03.개발/src/admin/controller/CoModifyController.java
@@ -338,7 +338,7 @@ public class CoModifyController extends WindowAdapter implements MouseListener, 
 					}
 					
 					msgCenter("회사 정보가 삭제되었습니다.");
-					au.sendLog("["+civo.getCoNum()+"] 회사 정보 삭제");
+					au.sendLog("["+civo.getCoNum()+"] 삭제");
 					cmv.dispose();
 					ammc.setCo();
 				}
